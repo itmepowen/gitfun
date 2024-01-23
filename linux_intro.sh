0eaf4ead8003c35dfb04b1b5baee15cf1727ea82
raopawan0309@gmail.com <raopawan0309@gmail.com>
2024-01-23 22:57:27 +0530

    rm mv cp

--- linux_intro.sh
+++ linux_intro.sh
@@ -12,3 +12,26 @@ cat /etc/os-release
 # create dev folder
 mkdir dev
 
+# create textfile.txt
+touch textfile.txt
+
+# create files in range
+touch textfile{1..10}.txt
+
+# copy files
+cp filename /locationtocopy/
+
+# copy folder
+cp -r folder /dir_where_to_copy
+
+# move file
+mv filename /locationtomove
+
+# renaming file using mv
+mv oldfilename newfilename
+
+# move all txt file
+mv *.txt textdir/
+
+# remove all txt file inside folder
+rm -r /home/vagrant/textdir/*.txt
