63848f21ba9ff4ac8a6bd4090eaa40a857b54f90
raopawan0309@gmail.com <raopawan0309@gmail.com>
2024-01-23 23:30:47 +0530

    vim commands

---
+++ vim.sh
@@ -0,0 +1,20 @@
+# VIM
+
+
+# install vim in centos
+
+
+# create a file using vim
+vim firstfile.txt
+
+# vim always open in command mode
+# for using insert mode need to press i
+# after coming to insert mode you can use it to write inside file
+# for saving we need to go back to command mode
+# press esc to go back to command mode
+
+# press :w to enter into extended write mode
+# press :q to save and exit from vim
+
+# :wq save and exit
+# :q!  for exit without saving
