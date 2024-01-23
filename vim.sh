892d74e528c4a99ba51f4a76b45321d2ae50b981
raopawan0309@gmail.com <raopawan0309@gmail.com>
2024-01-23 23:55:38 +0530

    vim commands

--- vim.sh
+++ vim.sh
@@ -7,6 +7,12 @@ sudo yum install vim -y
 # create a file using vim
 vim firstfile.txt
 
+
+# vim have 3 modes
+# 1. command mode
+# 2. Insert mode (edit)
+# 3. extended mode
+
 # vim always open in command mode
 # for using insert mode need to press i
 # after coming to insert mode you can use it to write inside file
@@ -18,3 +24,19 @@ vim firstfile.txt
 
 # :wq save and exit
 # :q!  for exit without saving
+
+
+# gg for going to first line
+# G for going to last line
+# :se nu  for setting line numbers
+
+# yy to copy line
+# 4yy to copy 4 lines
+# p to paste below
+# P to paste above
+# dd to delete line
+# 4dd to cut 4 lines
+# u to undo
+
+# /name for search name in file
+# enter and press n to go to next search
