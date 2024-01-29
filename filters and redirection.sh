876cb416e0f05175ea0bb314a17253d59719ba17
raopawan0309@gmail.com <raopawan0309@gmail.com>
2024-01-29 16:10:42 +0530

    filter and redirections

--- filters and redirection.sh
+++ filters and redirection.sh
@@ -1,19 +1,19 @@
-search in file 
+# search in file 
 grep searchstring filename
-it will return line in which searchstring  is  present
+# it will return line in which searchstring  is  present
 
 
 grep -i firewall anaconda-ks.cfg
 
 
-to search word in all files
+# to search word in all files
 grep -r firewall *
 
 
-searchinf for config
+# searching for config
 grep -R SELINUX  /etc/*
 
-Don't show lines with this word 
+# Don't show lines with this word 
 grep -vi SELINUX  /etc/*
 
 
@@ -40,3 +40,18 @@ cut -d: -f1 long_path_of_file
 search and replace word1 with word2 in multifiles
 sed 's/word1/word2/g' filename 
 sed -i 's/word1/word2/g' *.txt 
+
+
+# Redictions
+# we are redirecting output to some file using >
+ls > demo.txt
+# if we use single > it will overwrite but if we use >> it will append it.
+
+
+# count number of lines
+wc -l
+
+# if we want to pipe it we can use |
+#  now we will count number of files in /etc dir
+ls | wc -l
+
